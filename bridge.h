3a2a5f627e6beb971c409fddce44945cb2ba10ad
Richard Greene <RichardGreene@Richards-MacBook-Pro.local>
2017-08-29 20:02:40 -0400

    integrated AFNetworking and a bridge.h file

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  ComicBook
+//
+//  Created by Richard Greene on 8/28/17.
+//  Copyright © 2017 Ison Apps. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+#endif /* bridge_h */
